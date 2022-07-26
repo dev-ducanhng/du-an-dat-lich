7a9022055e0dc5eb1f55041eefea35cc6b6db782
anhtuan2810 <jos.anhtuan99@gmail.com>
2022-07-26 15:18:15 +0700

    Feat: Booking history

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -6,7 +6,10 @@ use App\Models\Booking;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HistoryController extends Controller
 {
@@ -23,19 +26,19 @@ class HistoryController extends Controller
 
         if (auth()->user()) {
             $dataBookings = Booking::where('stylist', auth()->id())->orWhere('user_id', auth()->id())->with([
+                'user',
+                'bookingService' => function ($queryBookingService) {
+                    $queryBookingService->with('service');
+                },
+                'bookingDate', 'Stylist', 'discount'])->paginate(10)->withQueryString();
+        } else {
+            if ($request->filled('phone_number')) {
+                $dataBookings = Booking::where('phone_number', 'LIKE', "%{$searchByPhoneNumber}%")->with([
                     'user',
                     'bookingService' => function ($queryBookingService) {
                         $queryBookingService->with('service');
                     },
                     'bookingDate', 'Stylist', 'discount'])->paginate(10)->withQueryString();
-        } else {
-            if ($request->filled('phone_number')) {
-                $dataBookings = Booking::where('phone_number', 'LIKE', "%{$searchByPhoneNumber}%")->with([
-                        'user',
-                        'bookingService' => function ($queryBookingService) {
-                            $queryBookingService->with('service');
-                        },
-                        'bookingDate', 'Stylist', 'discount'])->paginate(10)->withQueryString();
             }
             if ($request->filled('code')) {
                 $dataBookings = Booking::where('customer_name', 'LIKE', "%{$searchByName}%")->with([
@@ -55,5 +58,25 @@ class HistoryController extends Controller
             }
         }
         return view('home.history', compact('dataBookings'));
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function updateBookingStatus(Request $request): RedirectResponse
+    {
+        try {
+            $booking = Booking::where('id', $request->input('booking_id'));
+            $booking->update([
+                'status' => $request->input('status'),
+            ]);
+
+            return redirect()->back()->with('success', 'Bạn đã cập nhật trạng thái đặt lịch thành công');
+        } catch (\Exception $exception){
+            Log::info($exception);
+
+            return redirect()->back()->with('error', 'Có lỗi hệ thống xảy ra. Vui lòng liên hệ với quản trị viên');
+        }
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.home')
 @section('content')
     <div class="hero-area hero-bg">
@@ -7,8 +6,14 @@
                 <div class="col-lg-12 text-center">
                     <div class="hero-text">
                         <div class="hero-text-tablecell">
-                            <div class="hero-btns">
-                                <a href="{{route('booking')}}" class="boxed-btn">Đặt lịch</a>
+                            <div class="hero-btns ">
+                                @if(! auth()->user())
+                                    <button data-toggle="modal" data-target="#exampleModalCenter"
+                                            class="btn-booking btn">Đặt lịch
+                                    </button>
+                                @else
+                                    <a href="{{route('booking')}}" class="btn-booking btn">Đặt lịch</a>
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -16,7 +21,24 @@
             </div>
         </div>
     </div>
-
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+         aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content" style="border: 1px solid #F28123 ">
+                <div class="modal-body">
+                    <a href="{{route('login')}}" class="btn btn-booking w-100 mt-2"> Đăng nhập (Bạn đã có tài khoản)</a>
+                    <a class="btn btn-booking w-100 mt-2">Đăng ký (Bạn chưa có tài khoản)</a>
+                    <a href="{{route('booking')}}" class="btn btn-booking w-100 mt-2">Đặt lịch không cần tài khoản</a>
+                </div>
+                <div class="text-center p-3">
+                    <button type="button" class="btn" data-dismiss="modal" style="border: 1px solid #F28123; color: #F28123">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- features list section -->
     <div class="container">
         <div class="feature-bg">
@@ -24,7 +46,7 @@
                 <div class="row">
                     <div class="col-lg-7">
                         <div class="featured-text">
-                            <h2 class="pb-3"> <span class="orange-text">Về chúng tôi</span></h2>
+                            <h2 class="pb-3"><span class="orange-text">Về chúng tôi</span></h2>
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 mb-4 mb-md-5">
                                     <div class="list-box d-flex">
@@ -80,7 +102,7 @@
                 <div class="col-lg-6 col-md-6 text-center">
                     <div class="single-product-item">
                         <div class="product-image">
-                            <a ><img src="assets/img/products/product-img-1.jpg" alt=""></a>
+                            <a><img src="assets/img/products/product-img-1.jpg" alt=""></a>
                         </div>
                         <h3>Tóc nữ</h3>
                         <p class="product-price"><span>Ưu đãi</span> -30% </p>
@@ -89,7 +111,7 @@
                 <div class="col-lg-6 col-md-6 text-center">
                     <div class="single-product-item">
                         <div class="product-image">
-                            <a ><img src="assets/img/products/product-img-2.jpg" alt=""></a>
+                            <a><img src="assets/img/products/product-img-2.jpg" alt=""></a>
                         </div>
                         <h3>Tóc nam</h3>
                         <p class="product-price"><span>Ưu đãi</span> 15% </p>
@@ -106,34 +128,35 @@
             <div class="row">
                 <div class="col-lg-8 offset-lg-2 text-center">
                     <div class="section-title">
-                        <h3><span class="orange-text">Dịch vụ</span> </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p>
+                        <h3><span class="orange-text">Dịch vụ</span></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet
+                            beatae optio.</p>
                     </div>
                 </div>
             </div>
 
             <div class="row">
                 @foreach ($models as $item)
-                <div class="col-lg-3 col-md-6 text-center">
-                    <div class="single-product-item">
-                        <div class="product-image">
-                            <a href="{{route('booking')}}"><img src="{{asset($item->image)}}" alt="{{asset($item->name)}}"></a>
-                        </div>
-                        <h3>{{$item->name}}</h3>
-                        <p class="product-price">  @if ($item->discount !='' || $item->discount != null)
-                            <span>Giảm giá {{$item->discount}}%</span>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="single-product-item">
+                            <div class="product-image">
+                                <a href="{{route('booking')}}"><img src="{{asset($item->image)}}"
+                                                                    alt="{{asset($item->name)}}"></a>
+                            </div>
+                            <h3>{{$item->name}}</h3>
+                            <p class="product-price">  @if ($item->discount !='' || $item->discount != null)
+                                    <span>Giảm giá {{$item->discount}}%</span>
                             <p>
-                                <span> <span  style="color: #F28123;font-size: 1.5rem" >{{$item->priceDiscount}} đ</span> <span style="color: rgb(214, 211, 211) ;font-size: 1.5rem;text-decoration: line-through"> {{$item->price}} đ</span> </span>
+                                <span> <span style="color: #F28123;font-size: 1.5rem">{{$item->priceDiscount}} đ</span> <span
+                                        style="color: rgb(214, 211, 211) ;font-size: 1.5rem;text-decoration: line-through"> {{$item->price}} đ</span> </span>
                             </p>
                             @endif</p>
                             <a href="{{route('booking')}}" class="cart-btn"> Đặt lịch</a>
+                        </div>
                     </div>
-                </div>
                 @endforeach
-               
-                
-            
-              
+
+
             </div>
         </div>
     </div>
@@ -141,18 +164,23 @@
 
 
     <!-- advertisement section -->
-    <div class="abt-section mb-100 mt-100" >
+    <div class="abt-section mb-100 mt-100">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-md-12">
                     <div class="abt-bg">
-                        <a href="https://www.youtube.com/watch?v=Nt1HZ3ml0Xs" class="video-play-btn popup-youtube"><i class="fas fa-play"></i></a>
+                        <a href="https://www.youtube.com/watch?v=Nt1HZ3ml0Xs" class="video-play-btn popup-youtube"><i
+                                class="fas fa-play"></i></a>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12">
                     <div class="abt-text">
                         <h2><span class="orange-text">Mẫu tóc nổi bật</span></h2>
-                        <p>Kiểu tóc side part vuốt rủ trở thành xu hướng “hot trend” trở lại dù không phải là kiểu tóc mới xuất hiện, nó vẫn được giới trẻ theo đuổi.Thông thường kiểu side part cổ điển sẽ chú trọng về những đường cắt gọn. Tuy nhiên, nếu bạn tóc mình trông hiện đại hơn thì có thể tỉa bớt tóc cho mỏng đi hoặc tỉa layer phần mái. Đây là phong cách luôn nằm đầu danh sách kiểu tóc được ưu chuộng nhất mọi thời đại.</p>
+                        <p>Kiểu tóc side part vuốt rủ trở thành xu hướng “hot trend” trở lại dù không phải là kiểu tóc
+                            mới xuất hiện, nó vẫn được giới trẻ theo đuổi.Thông thường kiểu side part cổ điển sẽ chú
+                            trọng về những đường cắt gọn. Tuy nhiên, nếu bạn tóc mình trông hiện đại hơn thì có thể tỉa
+                            bớt tóc cho mỏng đi hoặc tỉa layer phần mái. Đây là phong cách luôn nằm đầu danh sách kiểu
+                            tóc được ưu chuộng nhất mọi thời đại.</p>
                     </div>
                 </div>
             </div>
@@ -168,8 +196,9 @@
             <div class="row">
                 <div class="col-lg-8 offset-lg-2 text-center">
                     <div class="section-title">
-                        <h3><span class="orange-text">Tin tức</span> </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p>
+                        <h3><span class="orange-text">Tin tức</span></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet
+                            beatae optio.</p>
                     </div>
                 </div>
             </div>
@@ -177,43 +206,55 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6">
                     <div class="single-latest-news">
-                        <a href="single-news.html"><div class="latest-news-bg news-bg-1"></div></a>
+                        <a href="single-news.html">
+                            <div class="latest-news-bg news-bg-1"></div>
+                        </a>
                         <div class="news-text-box">
                             <h3><a href="single-news.html">You will vainly look for fruit on it in autumn.</a></h3>
                             <p class="blog-meta">
                                 <span class="author"><i class="fas fa-user"></i> Admin</span>
                                 <span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
                             </p>
-                            <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
-                            <a href="single-news.html" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
+                            <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi.
+                                Praesent vitae mattis nunc, egestas viverra eros.</p>
+                            <a href="single-news.html" class="read-more-btn">read more <i
+                                    class="fas fa-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="single-latest-news">
-                        <a href="single-news.html"><div class="latest-news-bg news-bg-2"></div></a>
+                        <a href="single-news.html">
+                            <div class="latest-news-bg news-bg-2"></div>
+                        </a>
                         <div class="news-text-box">
                             <h3><a href="single-news.html">A man's worth has its season, like tomato.</a></h3>
                             <p class="blog-meta">
                                 <span class="author"><i class="fas fa-user"></i> Admin</span>
                                 <span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
                             </p>
-                            <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
-                            <a href="single-news.html" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
+                            <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi.
+                                Praesent vitae mattis nunc, egestas viverra eros.</p>
+                            <a href="single-news.html" class="read-more-btn">read more <i
+                                    class="fas fa-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
                     <div class="single-latest-news">
-                        <a href="single-news.html"><div class="latest-news-bg news-bg-3"></div></a>
+                        <a href="single-news.html">
+                            <div class="latest-news-bg news-bg-3"></div>
+                        </a>
                         <div class="news-text-box">
                             <h3><a href="single-news.html">Good thoughts bear good fresh juicy fruit.</a></h3>
                             <p class="blog-meta">
                                 <span class="author"><i class="fas fa-user"></i> Admin</span>
                                 <span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
                             </p>
-                            <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
-                            <a href="single-news.html" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
+                            <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi.
+                                Praesent vitae mattis nunc, egestas viverra eros.</p>
+                            <a href="single-news.html" class="read-more-btn">read more <i
+                                    class="fas fa-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
@@ -228,3 +269,28 @@
     <!-- end latest news -->
 
 @endsection
+@push('style')
+    <style>
+        .btn-booking {
+            border: 1px solid #F28123;
+            border-radius: 2px;
+            outline: 0 !important;
+            box-shadow: none !important;
+            background: #F28123;
+            color: white !important;
+            width: 40%;
+            padding: 15px 20px;
+        }
+
+        .btn-booking:hover {
+            border: 1px solid #F28123;
+            border-radius: 2px;
+            outline: 0 !important;
+            box-shadow: none !important;
+            background: #FFFFFF;
+            color: #F28123 !important;
+            width: 40%;
+            padding: 15px 20px;
+        }
+    </style>
+@endpush
